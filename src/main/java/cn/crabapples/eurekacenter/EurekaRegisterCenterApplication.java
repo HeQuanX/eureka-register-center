701d074287ad40515e3e3b97ebeaad0ac2c47b3e
MiBook Deepin <crabapples.cn@gmail.com>
2021-09-01 22:56:59 +0800

    rename

--- src/main/java/cn/crabapples/eurekacenter/EurekaRegisterCenterApplication.java
+++ src/main/java/cn/crabapples/eurekacenter/EurekaRegisterCenterApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaCenterApplication {
+public class EurekaRegisterCenterApplication     {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaCenterApplication.class, args);
+        SpringApplication.run(EurekaRegisterCenterApplication.class, args);
     }
 
 }
